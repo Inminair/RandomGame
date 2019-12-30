63da4401b5b74d753f833ed34c88388294fd0257
BuildTools <unconfigured@null.spigotmc.org>
2019-12-31 00:47:42 +0900

    ppap

---
+++ RandomGame/Assets/Script/backgroundMove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class backgroundMove : MonoBehaviour
+{
+    public float move_speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.left * move_speed);
+        if (transform.position.x < -9) transform.Translate(Vector2.right * 9.58f);
+    }
+}
